ebe16424fc8bb6cbb7a065e6eabbfc2d3c2a079d
minjae <alswo8138@gmail.com>
2021-03-28 19:16:28 +0900

    make blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,60 @@
+import * as CryptoJS from "crypto-js";
 
 
-interface Human {
-    name : string;
-    age : number;
-    gender : string;
+class Block {
+    public index : number;
+    public hash : string;
+    public previousHash : string;
+    public data : string;
+    public timestamp : number;
+
+    static calculateBlockHash = (
+        index: number, 
+        previousHash: string, 
+        timestamp:number,
+        data: string
+        ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+
+    constructor(
+        index : number,
+        hash : string,
+        previousHash : string,
+        data : string,
+        timestamp : number
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
 
-const person = {
-    name : "mj",
-    age : 20,
-    gender : "male"
-}
+const genesisBlock : Block = new Block(1,"303030","","20210328",37);
 
+let blockchain : Block[] = [genesisBlock] ;
 
-const sayHi = (person:Human) :void=>{
-    console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender} `)
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTImeStamp = (): number => Math.round(new Date().getTime()/1000);
+
+const createNewBlock = (data:string) : Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex : number = previousBlock.index +1000;
+    const newTimeStamp : number = getNewTImeStamp();
+    const newHash : string = Block.calculateBlockHash(newIndex, previousBlock.hash,newTimeStamp,data);
+    const newBlock: Block = new Block(newIndex,newHash,previousBlock.hash, data, newTimeStamp
+    );
+    return newBlock;
 };
 
-sayHi(person);
+console.log(createNewBlock("hello"),createNewBlock("bye"))
+
 
 export {};  // 이 파일이 모듈이 되는걸 이해시키는 코드
 
-// 인자와 반환값에 대한 타입을 설정해줄 수 있다
+
 
